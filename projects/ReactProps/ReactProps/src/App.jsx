1c198d52696afd471ca8d8f8280f3eb31c60f5cc
Nicole M <nkane@cfl.rr.com>
2022-07-08 19:39:27 -0400

    Removed hard coded values.  Replaced with values read from contacts.js file.

--- projects/ReactProps/ReactProps/src/App.jsx
+++ projects/ReactProps/ReactProps/src/App.jsx
@@ -6,15 +6,13 @@ function App() {
     return (
         <div>
             <h1 className="heading">My Contacts</h1>
-            <Card name={contacts.name[0]}
-                imgURL="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-                phone="+123 456 789"
-                email="b@beyonce.com" />
-
-            <Card name="Jack Bauer"
-                imgURL="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-                phone="+987 654 321"
-                email="jack@nowhere.com" />
+            {contacts.map(({ name, imgURL, phone, email }) => (
+                <Card name={name}
+                    imgURL={imgURL}
+                    phone={phone}
+                    email={email} />
+                
+            ))}
          </div>
     );
 }
