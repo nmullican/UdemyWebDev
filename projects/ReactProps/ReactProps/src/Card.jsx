36da4e56d1244bbd4dd0b7fadbede2d05aa65109
Nicole M <nkane@cfl.rr.com>
2022-07-13 13:10:39 -0400

    Add Avatar

--- projects/ReactProps/ReactProps/src/Card.jsx
+++ projects/ReactProps/ReactProps/src/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar"
 import './styles.css'
 
 function Card(props) {
     return <div className="card">
         <div className="top">
             <h2 className="name"> {props.name}</h2>
-            <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+            <Avatar imgURL={props.imgURL} />
         </div>
         <div className="bottom">
             <p>{props.phone}</p>
