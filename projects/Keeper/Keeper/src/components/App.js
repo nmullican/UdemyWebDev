22b3413d463e9c56d40a2ca51becfb816edc65da
Nicole M <nkane@cfl.rr.com>
2022-07-13 12:58:36 -0400

    Initial files

---
+++ projects/Keeper/Keeper/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+import '../styles.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
