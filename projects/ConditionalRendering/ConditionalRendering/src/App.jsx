9593cd1065e4b1a48a2b4724759b2f2d997d88a6
Nicole M <nkane@cfl.rr.com>
2022-07-14 10:42:14 -0400

    added comment

--- projects/ConditionalRendering/ConditionalRendering/src/App.jsx
+++ projects/ConditionalRendering/ConditionalRendering/src/App.jsx
@@ -5,6 +5,9 @@ var userIsRegistered = false;
 var buttonType = "Register";
 var confirmPW = true;
 
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
 userIsRegistered === true ? buttonType = "Login" : buttonType = "Register";
 
 //2. Only show the Confirm Password input if userIsRegistered is false.
