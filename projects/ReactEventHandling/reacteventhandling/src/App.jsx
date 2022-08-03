af23a7f639fc77d08e29b8360d49c0bc77c58e5a
Nicole M <nkane@cfl.rr.com>
2022-08-03 08:20:38 -0400

    Changed hover colors

--- projects/ReactEventHandling/reacteventhandling/src/App.jsx
+++ projects/ReactEventHandling/reacteventhandling/src/App.jsx
@@ -8,10 +8,7 @@ function App() {
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-          <button style={{
-              backgroundColor: hover ? 'salmon' : '',
-              color: hover ? 'white' : '',
-          }}
+          <button style={{backgroundColor: hover ? "black" : "white"}}
               onMouseOver={() => setHover(true)}
               onMouseOut={() => setHover(false)}>Submit</button>          
     </div>
