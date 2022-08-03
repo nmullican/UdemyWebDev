34a6cf95690600fd91622ca8904d0af59b92342d
Nicole M <nkane@cfl.rr.com>
2022-08-03 11:04:39 -0400

    Update name when submit if clicked.

--- projects/ReactForms/reactforms/src/App.jsx
+++ projects/ReactForms/reactforms/src/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [name, setName] = useState("");
+    const [subName, setSubName] = useState("");
+
+    function handleChange(event) {
+        setName(event.target.value);
+
+    }
+
+    function submitClicked(event) {
+        console.log("submitted ");
+        setSubName(name);
+    }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+          <h1>Hello {subName}</h1>
+          <input
+              onChange={handleChange}
+              type="text"
+              placeholder="What's your name?"
+              value={name}
+          />
+          <button onClick={submitClicked}>Submit</button>
     </div>
   );
 }
